33c625d47ba1b863614be0b58fd386f98bf84c02
Joao Gonçalves <joao.f.o.goncalves@gmail.com>
2021-03-04 22:04:09 +0000

    add social-django

--- aim4/settings.py
+++ aim4/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd Party
+    'social_django',
+
+    # Project apps
     'aim4.users',
 ]
 
