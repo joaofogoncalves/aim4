66f416d318d94a49b3e7ba3c2bc55bf8e4b209d1
Joao Gonçalves <joao.f.o.goncalves@gmail.com>
2021-03-05 23:11:11 +0000

    challenges join type

--- aim4/challenges/models.py
+++ aim4/challenges/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from aim4.mixins import BaseModel
 from aim4.users.models import User
 
+
 class Challenge(BaseModel):
+
+    class JoinTypes(models.TextChoices):
+        OPEN = 'OP', 'Opem'
+        APPROVAL = 'AP', 'Approval'
+        INVITE = 'IN', 'Invite'
+        CLOSED = 'CL', 'Closed'
 
     class Meta:
         verbose_name_plural = 'Challenges'
@@ -11,6 +18,9 @@ class Challenge(BaseModel):
     target_name = models.CharField('Target', max_length=250, null=False, blank=False)
     start_date = models.DateField('Start date', null=False, blank=False)
     target_date = models.DateField('Target date', null=True, blank=True)
+    public = models.BooleanField('Public', default=False)
+
+    join_type = models.CharField(max_length=2, choices=JoinTypes.choices,default=JoinTypes.OPEN )
 
     members = models.ManyToManyField(User, through='Membership', related_name='challenges')
 
@@ -25,6 +35,7 @@ class Membership(BaseModel):
 
     member = models.ForeignKey(User, related_name='memberships', on_delete=models.CASCADE, null=True)
     challenge = models.ForeignKey(Challenge, related_name='memberships', on_delete=models.CASCADE, null=True, blank=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = 'Memberships'
