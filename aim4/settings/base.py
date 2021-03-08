1bb8db5b33480ee139cafe7cc295227de562872d
Joao Gonçalves <joao.f.o.goncalves@gmail.com>
2021-03-08 18:38:51 +0000

    typo

--- aim4/settings/base.py
+++ aim4/settings/base.py
@@ -170,7 +170,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    ('', path.join(BASE_DIR,'static')), #store site-specific media here.
+    ('', os.path.join(BASE_DIR,'static')), #store site-specific media here.
 )
 
 
