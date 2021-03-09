c481599814aa3857fd5da80074091d1dc6f40e61
Joao Gonçalves <joao.f.o.goncalves@gmail.com>
2021-03-09 09:25:25 +0000

    corp pages

--- aim4/urls.py
+++ aim4/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -34,5 +35,9 @@ urlpatterns = [
     path('challenges/', challenges, name='challenges',),
     url(r'^challenges/(?P<challenge_id>\d+)$', challenge_detail, name='challenge_detail'),
     url(r'^challenges/(?P<challenge_id>\d+)/join$', challenge_join, name='challenge_join'),
+
+    path('privacy_policy/', TemplateView.as_view(template_name='corp\privacy_policy.html')),
+    path('terms_of_service/', TemplateView.as_view(template_name='corp\terms_of_service.html')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
